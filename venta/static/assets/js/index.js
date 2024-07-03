7858c6af5f8e47d726715adc953cce399146f4f5
DanteGb13 <angelrengifo140@gmail.com>
2024-07-03 17:38:21 -0400

    mas cambios
    mas cambios

--- venta/static/assets/js/index.js
+++ venta/static/assets/js/index.js
@@ -9,20 +9,6 @@ document.addEventListener('DOMContentLoaded', (event) => {
   //finalizado"esto solo es el auto pase del slider"
   
 
-/*logout 
-const user = JSON.parse(localStorage.getItem('login_success')) || false
-if(!user){
-    window.location.href = 'login.html'
-}
-
-const logout = document.querySelector('#logout')
-
-logout.addEventListener('click', ()=>{
-    alert('Hasta pronto!')
-    localStorage.removeItem('login_success')
-    window.location.href = 'login.html'
-})*/
-/*Fin del logout*/
 
 /*Para el mapa*/
 function initMap() {
