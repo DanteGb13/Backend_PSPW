b22214e34e35ce5538980dd0c959535e9e679397
DanteGb13 <angelrengifo140@gmail.com>
2024-07-05 22:33:25 -0400

    mas cambios
    no

--- tienda/settings.py
+++ tienda/settings.py
@@ -127,12 +127,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "/CarritoApp/static/",
-]
-
-LOGOUT_REDIRECT_URL = 'registro/'
-
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
