187d863072216b21b9ad08b95a808670aa720b80
DanteGb13 <angelrengifo140@gmail.com>
2024-07-13 18:47:18 -0400

    mas
    mas

--- tienda/settings.py
+++ tienda/settings.py
@@ -113,7 +113,13 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es'
+LANGUAGES = [
+    ('es', 'Spanish'),
+    ('en', 'English'),
+    # Añade otros idiomas que necesites
+]
+
 
 TIME_ZONE = 'UTC'
 
